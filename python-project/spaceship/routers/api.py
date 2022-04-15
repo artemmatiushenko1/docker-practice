6684142925622c2f6a691768720c7181355e2a77
Artem Matiushenko <artom.matyushenko@gmail.com>
2022-04-15 16:10:38 +0300

    Implemented /matrices api endpoint

--- python-project/spaceship/routers/api.py
+++ python-project/spaceship/routers/api.py
@@ -1,8 +1,20 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
-
 @router.get('')
 def hello_world() -> dict:
-    return {'msg': 'Hello, World!'}
+    return {'msg': 'Hello, World'}
+
+@router.get('/matrices')
+def matrices() -> dict:
+    matrix_a = np.random.rand(10, 10)
+    matrix_b = np.random.rand(10, 10)
+    product = np.matmul(matrix_a, matrix_b)
+
+    return {
+        'matrix_a': matrix_a.tolist(),
+        'matrix_b': matrix_b.tolist(),
+        'product': product.tolist()
+    }
